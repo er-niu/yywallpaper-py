4f4303a1a49e1e6755d6fd3e906f3fa250d1362b
qianqian.niu <qianqian.niu@56qq.com>
2018-07-10 17:59:21 +0800

    项目部署，依赖环境

--- jobs/time_job.py
+++ jobs/time_job.py
@@ -6,9 +6,6 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from conf import read_conf
 from service import scan_upload_day, scan_pic_day
 import logging
-import sys
-sys.path.append('/usr/local/src/yywallpaper-py')
-
 
 logging.basicConfig()
 
@@ -18,14 +15,15 @@ def my_job():
     # 定时任务
     # 1. 根据当前日期爬取最新图片，并保存到/分类/日期/ 文件夹 2016-07-23
     # 2. 读取文件夹图片，保存到fastfds和mysql
-    #date = '2018-07-02'
+    # date = '2018-07-09'
     scan_pic_day.scan_day_picture(date)
     scan_upload_day.upload_pic(date)
 
 
 sched = BlockingScheduler()
 # 定时每天 22:22:22秒执行任务
-sched.add_job(my_job, 'cron', day_of_week='mon-sun', hour=read_conf.get_conf('job', 'hour'),
-              minute=read_conf.get_conf('job', 'minute'), second=read_conf.get_conf('job', 'second'))
+sched.add_job(my_job, 'cron', day_of_week='0-6', hour=read_conf.get_conf('job', 'hour'),
+              minute=read_conf.get_conf('job', 'minute'), second=read_conf.get_conf('job', 'second'),
+              end_date='2114-05-30')
 
 sched.start()
